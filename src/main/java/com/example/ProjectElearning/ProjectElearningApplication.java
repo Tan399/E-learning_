3d9cf891b9f40edf86d4f05f662002f9d42d8a8c
Tanmay.Jaiswal <jaiswaltanmay6@gmail.com>
2025-01-23 12:46:21 +0530

    initial commit

---
+++ src/main/java/com/example/ProjectElearning/ProjectElearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectElearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectElearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectElearningApplication.class, args);
+	}
+
+}
